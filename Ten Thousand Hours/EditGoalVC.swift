655935dc7933b807768f27aa0b6a9cf278661915
flfk <flfk@users.noreply.github.com>
2017-06-01 17:38:43 -0300

    View Controllers UI set up

---
+++ Ten Thousand Hours/EditGoalVC.swift
@@ -0,0 +1,31 @@
+//
+//  EditGoalVC.swift
+//  Ten Thousand Hours
+//
+//  Created by Felix Kramer on 1/6/17.
+//  Copyright © 2017 Felix Kramer. All rights reserved.
+//
+
+import UIKit
+
+class EditGoalVC: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK: - Actions
+    
+    
+    //MARK: - Navigation
+    
+    
+    //MARK: - Notification Handling
+    
+    
+}
---
+++ Ten Thousand Hours/MainVC.swift
@@ -0,0 +1,32 @@
+//
+//  MainVC.swift
+//  Ten Thousand Hours
+//
+//  Created by Felix Kramer on 1/6/17.
+//  Copyright © 2017 Felix Kramer. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    //MARK: - Actions
+    
+    
+    //MARK: - Navigation
+    
+    
+    //MARK: - Notification Handling
+
+
+}
+
